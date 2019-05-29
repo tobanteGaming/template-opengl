bffde97c21cc9edbcf9a380fa79993b00f42d0d0
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-05-29 16:43:07 +0200

    Cleanup shader class

--- examples/include/shader.hpp
+++ examples/include/shader.hpp
@@ -1,99 +1,110 @@
 #pragma once
 
-#include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
 
 #include <GL/glew.h>
 
+/**
+ * @brief Represents an OpenGL program (vertex & fragment shader)
+ */
 class Shader
 {
+private:
+    unsigned int m_id;
+
 public:
-    unsigned int ID;
-    // constructor generates the shader on the fly
-    // ------------------------------------------------------------------------
+    /**
+     * @brief Constructor
+     *
+     * Takes sources for a vertex & fragment shader
+     */
     Shader(const char* vertexSource, const char* fragmentSource)
     {
-        const char* vShaderCode = vertexSource;
-        const char* fShaderCode = fragmentSource;
-        // 2. compile shaders
-        unsigned int vertex, fragment;
-        // vertex shader
+        // Vertex
+        unsigned int vertex;
         vertex = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glShaderSource(vertex, 1, &vertexSource, nullptr);
         glCompileShader(vertex);
         checkCompileErrors(vertex, "VERTEX");
-        // fragment Shader
+
+        // Fragment
+        unsigned int fragment;
         fragment = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glShaderSource(fragment, 1, &fragmentSource, nullptr);
         glCompileShader(fragment);
         checkCompileErrors(fragment, "FRAGMENT");
-        // shader Program
-        ID = glCreateProgram();
-        glAttachShader(ID, vertex);
-        glAttachShader(ID, fragment);
-        glLinkProgram(ID);
-        checkCompileErrors(ID, "PROGRAM");
-        // delete the shaders as they're linked into our program now and no
+
+        // Program
+        m_id = glCreateProgram();
+        glAttachShader(m_id, vertex);
+        glAttachShader(m_id, fragment);
+        glLinkProgram(m_id);
+        checkCompileErrors(m_id, "PROGRAM");
+
+        // Delete the shaders as they're linked into our program now and no
         // longer necessary
         glDeleteShader(vertex);
         glDeleteShader(fragment);
     }
-    // activate the shader
-    // ------------------------------------------------------------------------
-    void use() { glUseProgram(ID); }
-    // utility uniform functions
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Activates the shader
+     */
+    void use() { glUseProgram(m_id); }
+
+    /**
+     * @brief Returns the id
+     */
+    unsigned int getID() { return m_id; }
+
+    /**
+     * @brief Sets a uniform bool
+     */
     void setBool(const std::string& name, bool value) const
     {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+        glUniform1i(glGetUniformLocation(m_id, name.c_str()), (int)value);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a uniform int
+     */
     void setInt(const std::string& name, int value) const
     {
-        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+        glUniform1i(glGetUniformLocation(m_id, name.c_str()), value);
     }
-    // ------------------------------------------------------------------------
+
+    /**
+     * @brief Sets a uniform float
+     */
     void setFloat(const std::string& name, float value) const
     {
-        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+        glUniform1f(glGetUniformLocation(m_id, name.c_str()), value);
     }
 
 private:
-    // utility function for checking shader compilation/linking errors.
-    // ------------------------------------------------------------------------
-    void checkCompileErrors(unsigned int shader, std::string type)
+    void checkCompileErrors(unsigned int shader_id, std::string type)
     {
         int success;
         char infoLog[1024];
         if (type != "PROGRAM")
         {
-            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
             if (!success)
             {
-                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-                std::cout
-                    << "ERROR::SHADER_COMPILATION_ERROR of type: " << type
-                    << "\n"
-                    << infoLog
-                    << "\n -- "
-                       "--------------------------------------------------- -- "
-                    << std::endl;
+                glGetShaderInfoLog(shader_id, 1024, nullptr, infoLog);
+                std::string err = "ERROR::SHADER_COMPILATION_ERROR of type: ";
+                std::cout << err << type << "\n" << infoLog << '\n';
             }
         }
         else
         {
-            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            glGetProgramiv(shader_id, GL_LINK_STATUS, &success);
             if (!success)
             {
-                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-                std::cout
-                    << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n"
-                    << infoLog
-                    << "\n -- "
-                       "--------------------------------------------------- -- "
-                    << std::endl;
+                glGetProgramInfoLog(shader_id, 1024, nullptr, infoLog);
+                std::string err = "ERROR::PROGRAM_LINKING_ERROR of type: ";
+                std::cout << err << type << "\n" << infoLog << '\n';
             }
         }
     }
