c1f633f80f23828bc0a109bdd566f3b46cf4d272
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-06-04 23:35:42 +0200

    Add application class to tunnel

---
+++ tunnel/include/application.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <array>
+#include <string>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+namespace tobanteGaming
+{
+class Application
+{
+public:
+    Application(std::string name);
+    ~Application();
+
+    void Init();
+    void Run();
+
+private:
+    std::string m_name;
+    GLFWwindow* m_window;
+};
+}  // namespace tobanteGaming
---
+++ tunnel/src/application.cpp
@@ -0,0 +1,10 @@
+#include "application.hpp"
+
+namespace tobanteGaming
+{
+Application::Application(std::string name) : m_name(name) {}
+Application::~Application() {}
+
+void Application::Init() {}
+void Application::Run() {}
+}  // namespace tobanteGaming
