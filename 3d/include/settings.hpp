f764d077fa8d0efdd092349dd479b2bbf0847b8e
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-06-05 14:40:02 +0200

    Add object_loader

--- 3d/include/settings.hpp
+++ 3d/include/settings.hpp
@@ -2,3 +2,6 @@
 
 constexpr int WIDTH  = 1280;
 constexpr int HEIGHT = 720;
+
+constexpr char* RESOURCE_PATH
+    = R"(C:\Dev\tobanteGaming\projects\template-opengl\resources\3d\)";
--- 3d/main.cpp
+++ 3d/main.cpp
@@ -4,15 +4,18 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include "obj_loader.hpp"
 #include "settings.hpp"
 #include "shader.hpp"
 #include "shader_source.hpp"
-#include "shape.hpp"
+#include "shape/cube.hpp"
+#include "shape/shape.hpp"
 #include "warning.hpp"
 
 int main()
@@ -60,6 +63,103 @@ int main()
     Shader ourShader(VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
     tobanteGaming::Shape shape{};
 
+    // Initialize Loader
+    objl::Loader Loader;
+
+    // Load .obj File
+    bool loadout
+        = Loader.LoadFile(std::string(RESOURCE_PATH) + R"(obj\box_stack.obj)");
+
+    // If so continue
+    if (loadout)
+    {
+        // Create/Open e1Out.txt
+        std::ofstream file("e1Out.txt");
+
+        // Go through each loaded mesh and out its contents
+        for (int i = 0; i < Loader.LoadedMeshes.size(); i++)
+        {
+            // Copy one of the loaded meshes to be our current mesh
+            objl::Mesh curMesh = Loader.LoadedMeshes[i];
+
+            // Print Mesh Name
+            file << "Mesh " << i << ": " << curMesh.MeshName << "\n";
+
+            // Print Vertices
+            file << "Vertices:\n";
+
+            // Go through each vertex and print its number,
+            //  position, normal, and texture coordinate
+            for (int j = 0; j < curMesh.Vertices.size(); j++)
+            {
+                file << "V" << j << ": "
+                     << "P(" << curMesh.Vertices[j].Position.X << ", "
+                     << curMesh.Vertices[j].Position.Y << ", "
+                     << curMesh.Vertices[j].Position.Z << ") "
+                     << "N(" << curMesh.Vertices[j].Normal.X << ", "
+                     << curMesh.Vertices[j].Normal.Y << ", "
+                     << curMesh.Vertices[j].Normal.Z << ") "
+                     << "TC(" << curMesh.Vertices[j].TextureCoordinate.X << ", "
+                     << curMesh.Vertices[j].TextureCoordinate.Y << ")\n";
+            }
+
+            // Print Indices
+            file << "Indices:\n";
+
+            // Go through every 3rd index and print the
+            //	triangle that these indices represent
+            for (int j = 0; j < curMesh.Indices.size(); j += 3)
+            {
+                file << "T" << j / 3 << ": " << curMesh.Indices[j] << ", "
+                     << curMesh.Indices[j + 1] << ", " << curMesh.Indices[j + 2]
+                     << "\n";
+            }
+
+            // Print Material
+            file << "Material: " << curMesh.MeshMaterial.name << "\n";
+            file << "Ambient Color: " << curMesh.MeshMaterial.Ka.X << ", "
+                 << curMesh.MeshMaterial.Ka.Y << ", "
+                 << curMesh.MeshMaterial.Ka.Z << "\n";
+            file << "Diffuse Color: " << curMesh.MeshMaterial.Kd.X << ", "
+                 << curMesh.MeshMaterial.Kd.Y << ", "
+                 << curMesh.MeshMaterial.Kd.Z << "\n";
+            file << "Specular Color: " << curMesh.MeshMaterial.Ks.X << ", "
+                 << curMesh.MeshMaterial.Ks.Y << ", "
+                 << curMesh.MeshMaterial.Ks.Z << "\n";
+            file << "Specular Exponent: " << curMesh.MeshMaterial.Ns << "\n";
+            file << "Optical Density: " << curMesh.MeshMaterial.Ni << "\n";
+            file << "Dissolve: " << curMesh.MeshMaterial.d << "\n";
+            file << "Illumination: " << curMesh.MeshMaterial.illum << "\n";
+            file << "Ambient Texture Map: " << curMesh.MeshMaterial.map_Ka
+                 << "\n";
+            file << "Diffuse Texture Map: " << curMesh.MeshMaterial.map_Kd
+                 << "\n";
+            file << "Specular Texture Map: " << curMesh.MeshMaterial.map_Ks
+                 << "\n";
+            file << "Alpha Texture Map: " << curMesh.MeshMaterial.map_d << "\n";
+            file << "Bump Map: " << curMesh.MeshMaterial.map_bump << "\n";
+
+            // Leave a space to separate from the next mesh
+            file << "\n";
+        }
+
+        // Close File
+        file.close();
+    }
+    // If not output an error
+    else
+    {
+        // Create/Open e1Out.txt
+        std::ofstream file("e1Out.txt");
+
+        // Output Error
+        file << "Failed to Load File. May have failed to find it or it was not "
+                "an .obj file.\n";
+
+        // Close File
+        file.close();
+    }
+
     // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1
     // unit <-> 100 units
     glm::mat4 Projection = glm::perspective(
@@ -82,9 +182,23 @@ int main()
     // Remember, matrix multiplication is the other way around
     glm::mat4 mvp = Projection * View * Model;
 
+    double lastTime = glfwGetTime();
+    int nbFrames    = 0;
     // MAIN LOOP
     while (!glfwWindowShouldClose(window))
     {
+        // FPS
+        // Measure speed
+        double currentTime = glfwGetTime();
+        nbFrames++;
+        if (currentTime - lastTime >= 1.0)
+        {  // If last prinf() was more than 1 sec ago
+            // printf and reset timer
+            printf("%f ms/frame\n", 1000.0 / double(nbFrames));
+            nbFrames = 0;
+            lastTime += 1.0;
+        }
+
         // Render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
