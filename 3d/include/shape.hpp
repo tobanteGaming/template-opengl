0173644e2de9d5d05b355d18f6070364f10e7aef
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-06-05 13:53:21 +0200

    Extract shape in 3d

---
+++ 3d/include/shape.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <GL/glew.h>
+
+namespace tobanteGaming
+{
+class Shape
+{
+public:
+    Shape()
+    {
+        // 3D SETUP
+        // Set up vertex data (and buffer(s)) and configure vertex attributes
+        float vertices[] = {
+            0.5f,  0.5f,  0.0f,  // top right
+            0.5f,  -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f, 0.5f,  0.0f   // top left
+        };
+        unsigned int indices[] = {
+            // note that we start from 0!
+            0, 1, 3,  // first Triangle
+            1, 2, 3   // second Triangle
+        };
+        GLuint VBO, EBO;
+        glGenVertexArrays(1, &m_vao);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+        // bind the Vertex Array Object first, then bind and set vertex
+        // buffer(s), and then configure vertex attributes(s).
+        glBindVertexArray(m_vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices,
+                     GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                     GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+                              (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // note that this is allowed, the call to glVertexAttribPointer
+        // registered VBO as the vertex attribute's bound vertex buffer object
+        // so afterwards we can safely unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        // remember: do NOT unbind the EBO while a VAO is active as the bound
+        // element buffer object IS stored in the VAO; keep the EBO bound.
+        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        // You can unbind the VAO afterwards so other VAO calls won't
+        // accidentally modify this VAO, but this rarely happens. Modifying
+        // other VAOs requires a call to glBindVertexArray anyways so we
+        // generally don't unbind VAOs (nor VBOs) when it's not directly
+        // necessary.
+        glBindVertexArray(0);
+    }
+    GLuint getVAO() { return m_vao; }
+
+    void render()
+    {
+        glBindVertexArray(m_vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    }
+
+private:
+    GLuint m_vao;
+};
+}  // namespace tobanteGaming
--- 3d/main.cpp
+++ 3d/main.cpp
@@ -11,6 +11,7 @@
 #include "settings.hpp"
 #include "shader.hpp"
 #include "shader_source.hpp"
+#include "shape.hpp"
 #include "warning.hpp"
 
 int main()
@@ -23,8 +24,7 @@ int main()
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     // WINDOW
-    auto* window = glfwCreateWindow(WIDTH, HEIGHT, "Example: Getting Started",
-                                    nullptr, nullptr);
+    auto* window = glfwCreateWindow(WIDTH, HEIGHT, "3D", nullptr, nullptr);
     if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -56,97 +56,8 @@ int main()
             glViewport(0, 0, width, height);
         });
 
-    // SHADER DEBUG
-    int success;
-    char infoLog[512];
-
-    // VERTEX SHADER
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &VERTEX_SHADER_SOURCE, NULL);
-    glCompileShader(vertexShader);
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
-    }
-
-    // FRAGMENT SHADER
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &FRAGMENT_SHADER_SOURCE, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
-                  << infoLog << std::endl;
-    }
-
-    // PROGRAM
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // 3D SETUP
-    // Set up vertex data (and buffer(s)) and configure vertex attributes
-    float vertices[] = {
-        0.5f,  0.5f,  0.0f,  // top right
-        0.5f,  -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  // bottom left
-        -0.5f, 0.5f,  0.0f   // top left
-    };
-    unsigned int indices[] = {
-        // note that we start from 0!
-        0, 1, 3,  // first Triangle
-        1, 2, 3   // second Triangle
-    };
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s),
-    // and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-                 GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-                          (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // note that this is allowed, the call to glVertexAttribPointer registered
-    // VBO as the vertex attribute's bound vertex buffer object so afterwards we
-    // can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // remember: do NOT unbind the EBO while a VAO is active as the bound
-    // element buffer object IS stored in the VAO; keep the EBO bound.
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally
-    // modify this VAO, but this rarely happens. Modifying other VAOs requires a
-    // call to glBindVertexArray anyways so we generally don't unbind VAOs (nor
-    // VBOs) when it's not directly necessary.
-    glBindVertexArray(0);
+    Shader ourShader(VERTEX_SHADER_SOURCE, FRAGMENT_SHADER_SOURCE);
+    tobanteGaming::Shape shape{};
 
     // STATS
     int nrAttr;
@@ -160,17 +71,14 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Draw triangle
-        glUseProgram(shaderProgram);
-
-        // Animate color
+        // Triangle Color
+        ourShader.use();
         const float timeValue  = static_cast<float>(glfwGetTime());
         const float greenValue = sin(timeValue) / 2.0f + 0.5f;
-        const int vColorLoc = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vColorLoc, 0.0f, greenValue, 0.0f, 1.0f);
+        ourShader.setFloat4("ourColor", greenValue);
 
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // Draw triangle
+        shape.render();
 
         // Check and call events and swap the buffers
         glfwSwapBuffers(window);
