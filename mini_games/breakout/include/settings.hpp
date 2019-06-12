f613f6f79144832429bd8848003664e1f3bc9b8d
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-06-12 07:12:45 +0200

    Move tunnel & breakout to mini_games/

--- mini_games/breakout/include/settings.hpp
+++ mini_games/breakout/include/settings.hpp
@@ -18,7 +18,7 @@ constexpr std::uint16_t DEFAULT_SCREEN_HEIGHT = 1080;
 /**
  * @brief Game name.
  */
-constexpr char* GAME_NAME = "RemisenRun v0.1.0";
+constexpr const char* GAME_NAME = "RemisenRun v0.1.0";
 
 /**
  * @brief Path to resource folder (texture, shader & level).
